acb7e85813f03729c07e51d3eb59f8793c5b4ca5
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-04 19:09:46 +0500

    Add src\components\Projects.js

---
+++ src\components\Projects.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Projects() {
+  return (
+    <div className="projects">
+      <div className="project">
+        <h3>Project 1</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </div>
+      <div className="project">
+        <h3>Project 2</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </div>
+      <div className="project">
+        <h3>Project 3</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </div>
+    </div>
+  );
+}
+
+export default Projects;
